811778cf7ee7827b4e625c0cfc1ba9ef87830e79
Suren Safaryan <surensaf@gmail.com>
2015-09-29 02:46:17 -0700

    Deleted unused imports

--- app/src/main/java/com/safaryan/suren/myappportfolio/MainActivity.java
+++ app/src/main/java/com/safaryan/suren/myappportfolio/MainActivity.java
@@ -1,18 +1,12 @@
 package com.safaryan.suren.myappportfolio;
 
-import android.app.ActionBar;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);;;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
