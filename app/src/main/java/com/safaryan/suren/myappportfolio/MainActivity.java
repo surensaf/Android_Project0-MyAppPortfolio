5ca1738d70da6c73e881968a59889fdeebcf4b23
Suren Safaryan <surensaf@gmail.com>
2015-09-29 02:59:46 -0700

    Test

--- app/src/main/java/com/safaryan/suren/myappportfolio/MainActivity.java
+++ app/src/main/java/com/safaryan/suren/myappportfolio/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import android.Manifest.permission;
 
 public class MainActivity extends AppCompatActivity {
 
